7e868ca72319fe58a929c75939d6f88d8e5fb27d
DingDing <dingding@webos.dev>
2026-05-01 10:06:24 +0000

    chore: add .gitignore for kernel-rs, remove target from tracking

--- packages/os/kernel-rs/target/wasm32-unknown-unknown/release/deps/cfg_if-27bdecb88e8a17ca.d
+++
@@ -1,7 +0,0 @@
-/workspace/webos/packages/os/kernel-rs/target/wasm32-unknown-unknown/release/deps/cfg_if-27bdecb88e8a17ca.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-0.1.10/src/lib.rs
-
-/workspace/webos/packages/os/kernel-rs/target/wasm32-unknown-unknown/release/deps/libcfg_if-27bdecb88e8a17ca.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-0.1.10/src/lib.rs
-
-/workspace/webos/packages/os/kernel-rs/target/wasm32-unknown-unknown/release/deps/libcfg_if-27bdecb88e8a17ca.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-0.1.10/src/lib.rs
-
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-0.1.10/src/lib.rs:
--- packages/os/kernel-rs/target/wasm32-unknown-unknown/release/deps/memory_units-9a39a8e6fd72cfd9.d
+++
@@ -1,7 +0,0 @@
-/workspace/webos/packages/os/kernel-rs/target/wasm32-unknown-unknown/release/deps/memory_units-9a39a8e6fd72cfd9.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memory_units-0.4.0/src/lib.rs
-
-/workspace/webos/packages/os/kernel-rs/target/wasm32-unknown-unknown/release/deps/libmemory_units-9a39a8e6fd72cfd9.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memory_units-0.4.0/src/lib.rs
-
-/workspace/webos/packages/os/kernel-rs/target/wasm32-unknown-unknown/release/deps/libmemory_units-9a39a8e6fd72cfd9.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memory_units-0.4.0/src/lib.rs
-
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memory_units-0.4.0/src/lib.rs:
